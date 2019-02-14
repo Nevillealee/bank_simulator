e044451d67383503f8777854f2b68ad307bf1d4e
nevillealee <nevillealee@gmail.com>
2019-02-14 02:03:18 -0800

    adds account functions, float values for balance need precision adjustments to display 2 decimal places

--- src/csulb/cecs274/BankAccount.java
+++ src/csulb/cecs274/BankAccount.java
@@ -1,5 +1,7 @@
 package csulb.cecs274;
+import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.math.BigDecimal;
 
 
 public class BankAccount {
@@ -8,12 +10,12 @@ public class BankAccount {
     }
      private BankCustomer owner;
      private BankAccountType type;
-     private double balance;
+     private float balance;
      private float rate;
      private LocalDate startDate;
 
     // default constructor
-    public BankAccount(BankCustomer owner, BankAccountType type, double balance, float rate, LocalDate startDate ) {
+    public BankAccount(BankCustomer owner, BankAccountType type, float balance, float rate, LocalDate startDate ) {
         this.owner = owner;
         this.type  = type;
         this.balance = balance;
@@ -21,7 +23,7 @@ public class BankAccount {
         this.startDate = startDate;
     }
     // Type of account is optional, default value is checking
-    public BankAccount(BankCustomer owner, double balance, float rate, LocalDate startDate ) {
+    public BankAccount(BankCustomer owner, float balance, float rate, LocalDate startDate ) {
         this.owner = owner;
         this.type  = BankAccountType.CHECKING;
         this.balance = balance;
@@ -29,7 +31,7 @@ public class BankAccount {
         this.startDate = startDate;
     }
     // Date the account is opened and if not provided its initial value is the current date
-    public BankAccount(BankCustomer owner, BankAccountType type, double balance, float rate) {
+    public BankAccount(BankCustomer owner, BankAccountType type, float balance, float rate) {
         this.owner = owner;
         this.type  = type;
         this.balance = balance;
@@ -37,7 +39,7 @@ public class BankAccount {
         this.startDate = LocalDate.now();
     }
     //Interest rate is optional, default is lower bound of range of appropriate account type
-    public BankAccount(BankCustomer owner, BankAccountType type, double balance, LocalDate startDate ) {
+    public BankAccount(BankCustomer owner, BankAccountType type, float balance, LocalDate startDate ) {
         this.owner = owner;
         this.type  = type;
         this.balance = balance;
@@ -57,4 +59,59 @@ public class BankAccount {
         }
     }
 
+    public BankAccountType getType() {
+        return this.type;
+    }
+    public BankCustomer getOwner() {
+        return this.owner;
+    }
+    public double getBalance() {
+        return this.balance;
+    }
+    public void deposit(float credit){
+        if (credit > 0) {
+            this.balance += credit;
+            System.out.println("Deposited: $" + credit);
+            System.out.println("Balance now: $" + this.balance);
+        }
+        else {
+            System.out.println("Invalid amount entered!");
+        }
+    }
+    public void withdraw(float debit){
+        if(debit > 0 && this.balance >= debit) {
+            this.balance -= debit;
+            System.out.println("withdrew: $" + debit);
+            System.out.println("Balance now: $" + this.balance);
+        } else {
+            System.out.println("Insufficient funds!");
+        }
+    }
+    public void transfer(float transferAmt, BankAccount recipient ){
+        if(transferAmt > 0 && this.balance >= transferAmt && this.owner == recipient.owner) {
+            this.balance -= transferAmt;
+            recipient.balance += transferAmt;
+            System.out.println("Transfer complete!");
+            System.out.println("Recipient balance now: " + recipient.getBalance());
+        } else {
+            System.out.println("Error please try again..");
+        }
+    }
+    public void accrueInterest() {
+        float principle = this.balance;
+        float interest = principle * (this.rate/100);
+        BigDecimal bd = new BigDecimal(Float.toString(interest + principle));
+        bd = bd.setScale(2, RoundingMode.HALF_DOWN);
+        this.balance = bd.floatValue();
+        System.out.println("Interest earned: " + Math.round(interest * 100.0)/ 100.0);
+        System.out.println("New balance: " + bd);
+    }
+    // change this.owner to this.owner.getName() after BankCustomer class code completed
+    public String toString(){
+        if (this.type !=BankAccountType.CHECKING) {
+            return this.type + " with balance $" + String.format("%,.2f", this.balance) + " owned by " + this.owner + " interest rate: " + this.rate + "%";
+        } else {
+            return this.type + " with balance $" + String.format("%,.2f", this.balance) + " owned by " + this.owner;
+        }
+    }
 }
--- src/csulb/cecs274/Banking.java
+++ src/csulb/cecs274/Banking.java
@@ -1,10 +1,19 @@
 package csulb.cecs274;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Banking {
     public static void main(String[] args){
     // you have access to BankAccount or BankCustomer classes now
+        BankCustomer tuggie = new BankCustomer();
+        BankAccount tugChecking = new BankAccount(tuggie, 0, 0.225f, LocalDate.EPOCH);
+        BankAccount tugSavings = new BankAccount(tuggie, BankAccount.BankAccountType.SAVINGS,0.00f, 0.05f
+        );
 
+        System.out.println(tugChecking.getBalance());
+        tugChecking.deposit(10000.00f);
+        tugChecking.accrueInterest();
+        System.out.println(tugSavings.toString());
     }
 }
