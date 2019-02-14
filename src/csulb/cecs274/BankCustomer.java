4a8c80053859a6447e98b30394cf0a34bc2be8a8
linettemurillo <murillo.linette@gmail.com>
2019-02-14 02:33:33 -0800

    initial commit

--- src/csulb/cecs274/BankCustomer.java
+++ src/csulb/cecs274/BankCustomer.java
@@ -1,4 +1,16 @@
 package csulb.cecs274;
-
+import java.time.LocalDate;
+import java.util.ArrayList;
 public class BankCustomer {
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+    private ArrayList<BankAccount> list;
+
+    public BankCustomer(String first, String last, LocalDate birthday) {
+        this.list =new ArrayList<>(5);
+    }
+    public int getNumberOfAccounts(){
+        return list.size();
+    }
 }
